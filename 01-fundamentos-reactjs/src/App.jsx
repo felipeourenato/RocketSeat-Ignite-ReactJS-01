ac241d8893fd50c7d2e1bfdac2d7b760fb84d0d5
felipeourenato <feliperenatcho@gmail.com>
2022-11-03 21:34:39 -0300

    Iniciando com React

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,10 @@
+import { Post } from './Post';
+
+export function App() {
+  return (
+    <div>
+      <Post author={'Felipe'} content={'lorem ipsum dolor sit amet'} />
+      <Post author={'Diego'} content={'ignite'} />
+    </div>
+  );
+}
