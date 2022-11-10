8399328b32c90afe9f89cf76b81e614c56a2cacb
felipeourenato <feliperenatcho@gmail.com>
2022-11-10 18:03:54 -0300

    Corrigindo a estrutura do projeto

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment() {
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/felipeourenato.png" />
@@ -22,7 +22,7 @@ export function Comment({ content }) {
             </button>
           </header>
 
-          <p>{content}</p>
+          <p>Muito bom Devon, parabéns!!</p>
         </div>
         <footer>
           <button>
